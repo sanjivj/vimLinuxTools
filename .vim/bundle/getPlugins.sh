98af7395a1ddd2d9d9f796128bde1d46b8d5f4e8
sanjivj <sanjivchristopher@gmail.com>
2015-08-25 15:26:51 +0000

    The getPlugins.sh script installs submodules into bundle.

---
+++ .vim/bundle/getPlugins.sh
@@ -0,0 +1,15 @@
+# This shell script automatically fetches and installs plugins for .vim.
+
+git clone https://github.com/ervandew/supertab.git
+git clone https://github.com/scrooloose/syntastic.git
+git clone https://github.com/scrooloose/nerdcommenter.git
+git clone https://github.com/scrooloose/nerdtree.git
+git clone https://github.com/bling/vim-airline.git
+git clone https://github.com/tpope/vim-fugitive.git
+git clone https://github.com/reinh/vim-makegreen.git
+git clone https://github.com/tpope/vim-markdown.git
+git clone https://github.com/flazz/vim-colorschemes.git
+git clone https://github.com/airblade/vim-gitgutter.git
+git clone https://github.com/jmcantrell/vim-virtualenv.git
+
+
